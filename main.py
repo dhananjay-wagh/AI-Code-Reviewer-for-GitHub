ef16e0676b55298aead54bfa0924073b8bc09a29
dhananjay-wagh <dhananjaywagh426@gmail.com>
2026-07-31 18:24:47 +0530

    Add Java class with variable declarations and output

---
+++ main.py
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;                      
+        double price = 19.99;              
+        char grade = 'A';                  
+        boolean isJavaFun = true;          
+        String greeting = "Hello, user!";  
+
+        System.out.println(greeting + " Age: " + age);
+    }
+}
